8305f636d19bfbe6a2d48cfec44c550f12baa5b2
DannisMa <d085770128@gmail.com>
2026-01-18 21:23:45 +0800

    add findByIdForUpdate in AccountRespository

--- src/main/java/com/dannis/banking_system/repository/AccountRepository.java
+++ src/main/java/com/dannis/banking_system/repository/AccountRepository.java
@@ -1,12 +1,22 @@
 package com.dannis.banking_system.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dannis.banking_system.model.Account;
+
+import jakarta.persistence.LockModeType;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByUserId(Long userId);//查某人所有帳戶
     Account findByAccountNumber(String accountNumber);//查詢特定帳號
+    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.id = :id")
+    Optional<Account> findByIdForUpdate(@Param("id") Long id);
 }
